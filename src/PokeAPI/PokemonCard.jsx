ea5676c4d84918205cd0dba04b6ba61a7f1f1951
Joakim Villo <villokodehode@gmail.com>
2023-03-28 19:30:53 +0200

    styling fix

--- src/PokeAPI/PokemonCard.jsx
+++ src/PokeAPI/PokemonCard.jsx
@@ -5,7 +5,7 @@ function PokemonCard(props) {
   return (
     <div className="p-4 bg-yellow-400">
       <button
-        className="bg-white btn p-2 mb-1 rounded-none border-red-700 border-2 shadow-xl text-black"
+        className="bg-white hover:bg-slate-200 btn p-2 mb-1 rounded-none border-red-700 border-2 shadow-xl text-black"
         onClick={() => setToggle(!toggle)}
       >
         {props.name}
@@ -20,9 +20,9 @@ function PokemonCard(props) {
         </a>
         {toggle && (
           <>
-            <div className="animate-ProjectPopUp bg-white border-4 border-red-700 shadow-2xl shadow-black rounded-lg p-8 fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-black">
+            <div className="animate-ProjectPopUp bg-white border-4  border-red-700 shadow-2xl shadow-black rounded-lg p-8 fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-black">
               <button
-                className="absolute top-2 right-2 m-2"
+                className="absolute  top-2 right-2 m-2"
                 onClick={() => setToggle(!toggle)}
               >
                 ❌
